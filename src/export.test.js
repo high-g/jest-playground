2780428718f086b90d28561036a32c7aa7c155fa
fumiya <altema1217bbfumi@live.jp>
2021-08-28 18:40:44 +0900

    Update

--- src/export.test.js
+++ src/export.test.js
@@ -3,3 +3,10 @@ import { sum } from './export'
 test('adds 1 + 2 = 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
+
+test('null', () => {
+  const n = null
+  expect(n).toBe(null)
+  expect(n).toBeNull()
+  expect(undefined).toBeUndefined()
+})
